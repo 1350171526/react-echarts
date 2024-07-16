7d690c209a56a43b10817ce54323180a36823e8d
Letian <1350171527@qq.com>
2024-07-16 16:21:37 +0800

    feat: 首屏加载动画完成

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,8 +7,12 @@ import * as datav from '@jiaminghi/data-view-react'
 import './style.scss'
 import { useEffect, useState } from "react"
 import { getBaseWeatherApi } from "@/apis/getWeather"
+import loadingMp4 from '@/assets/video/loading.mp4'
+import bgvideo from '@/assets/video/home-bgvideo3.mp4'
 
 function Home(){
+  const [isShowApp,setShowApp] = useState(false)
+  const [bgvideoShow,setBgvideoShow] = useState(false)
   const [baseWeather,setBaseWeather] = useState([])
   const getWeather = async () => {
     const getBaseWeather =await getBaseWeatherApi()
@@ -16,21 +20,42 @@ function Home(){
   }
   useEffect(()=>{
     getWeather()
+    let timer1 = setTimeout(()=>{
+      setShowApp(true)
+      setBgvideoShow(true)
+      clearTimeout(timer1)
+    },2100)
+    let timer2 = setTimeout(()=>{
+      setBgvideoShow(false)
+      console.log(bgvideoShow);
+      clearTimeout(timer2)
+    },5500)
   },[])
-  return <>
+  if(!isShowApp){
+    return <>
+      <div className="home-video">
+				<video muted autoPlay loop src={loadingMp4}></video>
+			</div>
+    </>
+  }else{
+    return <>
     <div className="content">
       <div className="bg"></div>
-        <datav.FullScreenContainer>
-          <Title></Title>
-          <div style={{display: 'flex',width: '100%'}}>
-            <div style={{flex: '0 1 25%'}}><MainLeft baseWeather={baseWeather}></MainLeft></div>
-            <div style={{flex: '0 1 50%'}}><Main></Main></div>
-            <div style={{flex: '0 1 25%'}}><MainRight baseWeather={baseWeather}></MainRight></div>
-          </div>
-          <Bottom baseWeather={baseWeather}></Bottom>
-        </datav.FullScreenContainer>
+      <datav.FullScreenContainer>
+        <Title></Title>
+        <div style={{display: 'flex',width: '100%'}}>
+          <div style={{flex: '0 1 25%'}}><MainLeft baseWeather={baseWeather}></MainLeft></div>
+          <div style={{flex: '0 1 50%'}}><Main></Main></div>
+          <div style={{flex: '0 1 25%'}}><MainRight baseWeather={baseWeather}></MainRight></div>
+        </div>
+        <Bottom baseWeather={baseWeather}></Bottom>
+      </datav.FullScreenContainer>
+      <div className="home-video" style={{display: bgvideoShow ? 'block' : 'none'}}>
+				<video autoPlay loop muted src={bgvideo}></video>
+			</div>
     </div>
-  </>
+    </>
+  }
 }
 
 export default Home 
